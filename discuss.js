1aeeac660c508de9e30fdc48a055ffe03f9c01e2
Arnav Sharma <arnav1776@gmail.com>
2022-03-18 00:04:36 +0530

    localStorage

--- discuss.js
+++ discuss.js
@@ -1,29 +1,80 @@
-var submitQuestion = document.getElementById("submitBtm")
+var submitQuestionNode = document.getElementById("submitBtn");
+var questionTitleNode = document.getElementById("subject");
+var questionDescriptionNode = document.getElementById("question");
+var allQuestionsListNode = document.getElementById("dataList");
+
 
 // submit button
 
-submitQuestion.addEventListener("click", onQuestionSubmit)
+submitQuestionNode.addEventListener("click", onQuestionSubmit);
 
 function onQuestionSubmit()
 {
-  addQuestinToPanel();
+  var question = {
+    title: questionTitleNode.value,
+    description: questionDescriptionNode.value,
+  }
+  saveQuestion(question);
+  addQuestinToPanel(question);
 }
 
 // save to storage
 
-questions = [ { title: "", description: ""}]
-
-function saveQuestion()
+function saveQuestion(question)
 {
-    localStorage.setItem("todos", JSON.stringify());
+  // get all questions
+  let allQuestions = localStorage.getItem("questions");
+
+  if(allQuestions)
+  {
+    allQuestions = JSON.parse(allQuestions);
+  }
+  else
+  {
+    allQuestions = [];
+  }
+
+  allQuestions.push(question)
+
+  localStorage.setItem("questions", JSON.stringify(allQuestions));
 }
 
 // append question to left pane
 
-function addQuestinToPanel()
+function addQuestinToPanel(question)
+{
+  var questionContainer = document.createElement("div");
+
+  var newQuestionTitleNode = document.createElement("h4");
+  newQuestionTitleNode.innerHTML = question.title;
+  questionContainer.appendChild(newQuestionTitleNode);
+
+  var newQuestionDescriptionNode = document.createElement("p");
+  newQuestionDescriptionNode.innerHTML - question.description;
+  questionContainer.appendChild(newQuestionDescriptionNode);
+
+  allQuestionsListNode.appendChild(questionContainer);
+
+  
+}
+
+// display question on right pane
+
+function onQuestionClick()
 {
 
 }
-// display question on right pane
+
 // submit response button
+
+function onResponseSubmit()
+{
+
+}
+
 // display response
+
+function addResponseInPanel()
+{
+
+}
